a1d3e43733024c5837826a243dce569350f64233
Juniplay <juniplay2016@gmail.com>
2022-07-08 20:27:42 -0300

    updated

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ showBtn.addEventListener("click", function (){
     if(pass_field.type === "password"){
         pass_field.type = "text";
         showBtn.textContent = "Ocultar";
-        showBtn.style.color = "#ff5a00";
+        //showBtn.style.color = "#ff5a00";
+        showBtn.style.color = "#222";
     }else{
         pass_field.type = "password";
         showBtn.textContent = "Mostrar";
