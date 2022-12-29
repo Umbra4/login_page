0e92b531a736ad5104a2b380c8f5b3652056bb67
Juniplay <juniplay2016@gmail.com>
2022-12-29 20:51:27 -0300

    update

--- js/button.js
+++ js/button.js
@@ -2,4 +2,6 @@ const checkbox = document.querySelector(".checkbox")
 checkbox.addEventListener('change', () => {
     //mudar o tema do site
     document.body.classList.toggle('dark');
+    document.body
 })
+
